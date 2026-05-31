942db8889a678f30dedada5be09bbd5a4643e856
Meghana81238 <meghana81238@gmail.com>
2026-05-31 18:28:35 +0530

    Add game_art and game_database modules

---
+++ game_database.py
@@ -0,0 +1,99 @@
+data=[
+    {
+        'name':"jenny's lectures",
+        'follower_count':1,
+        'description':'youtuber',
+        'country':'india',
+    },
+
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 600,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count': 90,
+        'description': 'Prime Minister',
+        'country': 'India'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 250,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 300,
+        'description': 'Singer',
+        'country': 'USA'
+    },
+    {
+        'name': 'Elon Musk',
+        'follower_count': 180,
+        'description': 'Entrepreneur',
+        'country': 'USA'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 500,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Priyanka Chopra',
+        'follower_count': 120,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Bill Gates',
+        'follower_count': 150,
+        'description': 'Philanthropist',
+        'country': 'USA'
+    },
+    {
+        'name': 'Shah Rukh Khan',
+        'follower_count': 200,
+        'description': 'Actor',
+        'country': 'India'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 310,
+        'description': 'Singer',
+        'country': 'USA'
+    },
+    {
+        'name': 'Sundar Pichai',
+        'follower_count': 95,
+        'description': 'CEO of Google',
+        'country': 'India'
+    },
+    {
+        'name': 'Alia Bhatt',
+        'follower_count': 85,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 220,
+        'description': 'Actor & Wrestler',
+        'country': 'USA'
+    },
+    {
+        'name': 'Emma Watson',
+        'follower_count': 140,
+        'description': 'Actress',
+        'country': 'UK'
+    },
+    {
+        'name': 'A. R. Rahman',
+        'follower_count': 70,
+        'description': 'Music Composer',
+        'country': 'India'
+    }
+]
